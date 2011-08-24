9d7cc2c5e8581eb9eacd0d42f74dc9021b2ce5a5
Harry Mills <harry@haeg.in>
2011-08-24 16:19:01 +0100

    Added submodule init and update to the link-dotfiles script so it's not forgotten when you check out the repository and start using it straight away.

--- bin/link-dotfiles.sh
+++ bin/link-dotfiles.sh
@@ -13,4 +13,10 @@ for item in *~bin; do
         ln -s "${DOTDIR}/${item}" "${HOME}/.${item}"
     fi
 done
+
+# Set up the submodules now we're done so vim plugins are all working
+echo "Setting up the git submodules - until this is complete vim stuff probably won't work as expected."
+git submodule init
+git submodule update
+
 echo "\nFinished!"
