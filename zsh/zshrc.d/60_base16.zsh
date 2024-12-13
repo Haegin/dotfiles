61c6b2a4f96d6800bcb1190a648429a6698986a3
Harry Porter-Mills <harry.porter-mills@instacart.com>
2024-12-13 10:38:37 -0500

    Remove use of git submodules

--- zsh/zshrc.d/60_base16.zsh
+++ zsh/zshrc.d/60_base16.zsh
@@ -1,4 +1,9 @@
 # Base 16 colour schemes {{{
 BASE16_SHELL=$HOME/.config/base16-shell/
+
+if [ ! -d $BASE16_SHELL ]; then
+  git clone https://github.com/chriskempson/base16-shell $BASE16_SHELL
+fi
+
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 # }}}
--- zsh/zshrc.d/8_antidote.zsh
+++ zsh/zshrc.d/8_antidote.zsh
@@ -1,10 +1,10 @@
 if [ -f /opt/homebrew/opt/antidote/share/antidote/antidote.zsh ]; then
   source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
-elif [ -d ${ZDOTDIR:-~}/.antidote ]; then
-  source ${ZDOTDIR:-~}/.antidote/antidote.zsh
+elif [ -d ~/.antidote ]; then
+  source ~/.antidote/antidote.zsh
 else
-  git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
-  source ${ZDOTDIR:-~}/.antidote/antidote.zsh
+  git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
+  source ~/.antidote/antidote.zsh
 fi
 
 antidote load ~/.zsh/plugins
